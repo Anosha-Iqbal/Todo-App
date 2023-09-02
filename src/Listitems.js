fd09709b048dbceb9f0c79418b14517309b5421b
Anosha200 <anoshaiqbal200@gmail.com>
2023-09-02 01:38:12 -0700

    second commit

---
+++ src/Listitems.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Listitems = (props) => {
+    return (
+      <>
+        <div className="listcontainer">
+          <button
+            onClick={() => {
+                props.dltItem(props.id);
+              }}
+            className="dltTaskBtn"
+          >
+            X
+          </button>
+              <li className="listItem">{props.text}</li>
+        </div>
+      </>
+    );
+  };
+  
+  export default Listitems;
+
